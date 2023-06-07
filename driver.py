92208fc5a3325b8e17ad1d8d6492b9cb8df0e1e6
Arbish Akram <58566628+arbishakram@users.noreply.github.com>
2023-06-08 01:31:26 +0500

    Update driver.py

--- driver.py
+++ driver.py
@@ -15,39 +15,9 @@ from torch.backends import cudnn
 def str2bool(v):
     return v.lower() in ('true')
 
-def main(config):
-    # For fast training.
-    cudnn.benchmark = True
-
-    # Create directories if not exist.
-    if not os.path.exists(config.log_dir):
-        os.makedirs(config.log_dir)
-    if not os.path.exists(config.model_save_dir):
-        os.makedirs(config.model_save_dir)
-    if not os.path.exists(config.sample_dir):
-        os.makedirs(config.sample_dir)
-    if not os.path.exists(config.result_dir):
-        os.makedirs(config.result_dir)
-
-    # Data loader.   
-    dataset_loader = get_loader(config.image_dir, config.image_size, config.batch_size,
-                                 config.mode, config.num_workers)
-    
-
-    # Solver for training and testing StarGAN.
-    solver = Solver(dataset_loader, config)
-
-    if config.mode == 'train':       
-            solver.train()      
-    elif config.mode == 'test':       
-            solver.test()
-        
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    # Model configuration.
     parser.add_argument('--c_dim', type=int, default=7, help='dimension of domain labels (1st dataset)')   
     parser.add_argument('--image_size', type=int, default=128, help='image resolution')
     parser.add_argument('--g_conv_dim', type=int, default=64, help='number of conv filters in the first layer of G')
@@ -56,9 +26,6 @@ if __name__ == '__main__':
     parser.add_argument('--lambda_cls', type=float, default=1, help='weight for domain classification loss')
     parser.add_argument('--lambda_rec', type=float, default=1, help='weight for reconstruction loss')
     parser.add_argument('--lambda_gp', type=float, default=10, help='weight for gradient penalty')
-    
-    # Training configuration.
-    parser.add_argument('--dataset', type=str, default='RaFD', choices=['CelebA', 'RaFD', 'Both'])
     parser.add_argument('--batch_size', type=int, default=1, help='mini-batch size')
     parser.add_argument('--num_iters', type=int, default=200000, help='number of total iterations for training D')
     parser.add_argument('--num_iters_decay', type=int, default=100000, help='number of iterations for decaying lr')
@@ -67,24 +34,16 @@ if __name__ == '__main__':
     parser.add_argument('--n_critic', type=int, default=5, help='number of D updates per each G update')
     parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for Adam optimizer')
     parser.add_argument('--beta2', type=float, default=0.999, help='beta2 for Adam optimizer')
-    parser.add_argument('--resume_iters', type=int, default=48000, help='resume training from this step')
-
-    # Test configuration.
+    parser.add_argument('--resume_iters', type=int, default=None, help='resume training from this step')
     parser.add_argument('--test_iters', type=int, default=87000, help='test model from this step')
-    # /content/drive/MyDrive/Research/research_2021/dwarfgan+last+FAU+skip/dwarfgan+last+FAU+skip/sargan1/models
-    # Miscellaneous.
     parser.add_argument('--num_workers', type=int, default=1)
-    parser.add_argument('--mode', type=str, default='test', choices=['train', 'test'])
-    parser.add_argument('--use_tensorboard', type=str2bool, default=True)
-
-    # Directories.
-    testpath = '/content/drive/MyDrive/Research/dwarfgan_paper/dataset/RaFD/test/'
-    model_path = '/media/sia/Transcend/phdcsf18m002@account_data/Research/US-GAN/dwarfgan-20230118T142144Z-001/dwarfgan/'
-    parser.add_argument('--image_dir', type=str, default='/home/sia/Desktop/arbish_research_work/AUs_tfem/results_reggan_paper-feb-23/')
-    parser.add_argument('--log_dir', type=str, default='dwarfgan/logs')
-    parser.add_argument('--model_save_dir', type=str, default=model_path+'models')
-    parser.add_argument('--sample_dir', type=str, default='dwarfgan/samples')
-    parser.add_argument('--result_dir', type=str, default='./results_for_reggan_paper-feb24')
+    parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'])
+    parser.add_argument('--use_tensorboard', type=str2bool, default=False)
+    parser.add_argument('--image_dir', type=str, default='./dataset/')
+    parser.add_argument('--log_dir', type=str, default='usgan/logs')
+    parser.add_argument('--model_save_dir', type=str, default='usgan/models')
+    parser.add_argument('--sample_dir', type=str, default='usgan/samples')
+    parser.add_argument('--result_dir', type=str, default='usgan/results')
 
     # Step size.
     parser.add_argument('--log_step', type=int, default=10)
@@ -94,7 +53,27 @@ if __name__ == '__main__':
 
     config = parser.parse_args()
     print(config)
-    main(config)
     
-#44778432
-#2533440
+    
+def main(config):   
+    cudnn.benchmark = True
+
+    if not os.path.exists(config.log_dir):
+        os.makedirs(config.log_dir)
+    if not os.path.exists(config.model_save_dir):
+        os.makedirs(config.model_save_dir)
+    if not os.path.exists(config.sample_dir):
+        os.makedirs(config.sample_dir)
+    if not os.path.exists(config.result_dir):
+        os.makedirs(config.result_dir)
+
+    dataset_loader = get_loader(config.image_dir, config.image_size, config.batch_size,
+                                 config.mode, config.num_workers)    
+    solver = Solver(dataset_loader, config)
+
+    if config.mode == 'train':       
+            solver.train()      
+    elif config.mode == 'test':       
+            solver.test()
+    
+main(config)
